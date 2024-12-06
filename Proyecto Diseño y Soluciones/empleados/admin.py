8c5fbc334d6967375678a973c6d73a0c5729af03
Draxer21 <167593612+Draxer21@users.noreply.github.com>
2024-12-06 13:06:03 -0300

    Delete Proyecto Diseño y Soluciones directory

--- Proyecto Diseño y Soluciones/empleados/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Empleado, ContactoEmergencia, CargaFamiliar
-
-admin.site.register(Empleado)
-admin.site.register(ContactoEmergencia)
-admin.site.register(CargaFamiliar)
